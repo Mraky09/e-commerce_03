ece640ccba297b4d3d2c637baec1bccd51a7a4fd
HungNH <hungnh103@gmail.com>
2017-01-05 18:19:57 +0700

    Create Static Pages
    Sign up, sign out, login

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,16 @@ module ApplicationHelper
     base_title = "E-Commerce"
     page_title.empty? ? base_title : page_title + " | " + base_title
   end
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
