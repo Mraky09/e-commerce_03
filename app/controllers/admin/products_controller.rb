86f9975a4ac13a9241264104cdcd118337501492
Mraky09 <anhtnt09@gmail.com>
2017-01-10 10:04:46 +0700

    Admin search & filter product

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -2,11 +2,13 @@ class Admin::ProductsController < ApplicationController
   before_action :is_admin?
 
   def index
-    @products = Product.select("id", "name", "price", "category_id").includes(:category)
+    @search = Product.search params[:q]
+    @products = @search.result.select(:id, :name, :price, :category_id)
+      .includes(:category).order(created_at: :DESC).page params[:page]
   end
 
   def new
-    @categories = Category.select("id", "name", "depth")
+    @categories = Category.select(:id, :name, :depth)
     @product = Product.new
     respond_to do |format|
       format.html{render partial: "product_form", local: {product: @product}}
