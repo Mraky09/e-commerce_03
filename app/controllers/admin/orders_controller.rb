06035606dc9f31e0649a293fa585bbd2223ed862
HungNH <hungnh103@gmail.com>
2017-01-13 11:42:59 +0700

    [Admin][Manage Orders] View list, edit status

---
+++ app/controllers/admin/orders_controller.rb
@@ -0,0 +1,21 @@
+class Admin::OrdersController < ApplicationController
+  before_action :authenticate_user!, :is_admin?
+  load_and_authorize_resource
+  layout "admin"
+
+  def index
+    @orders = Order.order(created_at: :desc)
+      .page(params[:page]).per Settings.admin.orders_list.per_page
+  end
+
+  def update
+    unless params[:status].nil?
+      if @order.update_attributes status: params[:status].to_i
+        flash[:success] = t ".update_success"
+      else
+        flash[:notice] = t ".update_fail"
+      end
+      redirect_to admin_orders_path
+    end
+  end
+end
